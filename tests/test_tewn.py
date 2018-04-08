1ccf6177ffe370d78d835cb0e3638de07172053b
pundurs <pundurs@glhf.lv>
2018-04-08 20:34:39 +0300

    Update CLI test to handle cases when default input device isn't found.

--- tests/test_tewn.py
+++ tests/test_tewn.py
@@ -7,9 +7,15 @@ from tewn import cli
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
+
     result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'Input initialized.' in result.output
+    if result.exit_code == 0:
+        assert 'Input initialized.' in result.output
+    else:
+        # Travis CI test runner doesn't have a default audio input device set.
+        assert result.exit_code == 1
+        assert 'Couldn\'t get default' in result.output
+
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
